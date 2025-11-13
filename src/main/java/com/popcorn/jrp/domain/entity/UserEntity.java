4082697c73a1b18a9d9410b658deb75949c69277
Akira Satoh <akira31758421@gmail.com>
2025-11-13 10:15:28 +0700

    fixed: require role

--- src/main/java/com/popcorn/jrp/domain/entity/UserEntity.java
+++ src/main/java/com/popcorn/jrp/domain/entity/UserEntity.java
@@ -36,7 +36,7 @@ public class UserEntity extends BaseEntity implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name().toUpperCase()));
     }
 
     @Override
